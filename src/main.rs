0cfb62f822c59a16283af3766fcd1c9a4f1f6e90
aj <aj@softwarebyaj.com>
2019-12-02 03:16:30 +0000

    addded timing notes

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@
 extern crate day01;
 
 use std::fs;
+use std::time::{Instant};
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let now = Instant::now();
+
     let input: String  = fs::read_to_string("day01/input.data")?.parse()?;
     let v: Vec<u32> = input.lines().map(|x| x.parse::<u32>().unwrap()).collect();
 
     let w:u32 = v.iter().map(|x| day01::day01::mass_to_fuel(*x)).sum();
 
     println!("{}", w);
-    
+    println!("took {} us", now.elapsed().as_micros());
     Ok(())
 }
