9e4984d9552c2f56d86c1197d0aea3d44b9bdcf9
aj <aj@softwarebyaj.com>
2019-12-03 05:26:15 +0000

    all the test cases work but the answer is wrong

---
+++ day02/src/lib.rs
@@ -0,0 +1,49 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn ex1 () {
+        assert_eq!(crate::day02::run_program(vec![1,9,10,3,2,3,11,0,99,30,40,50], 0), [3500,9,10,70,2,3,11,0,99,30,40,50]);
+    }
+
+    #[test]
+    fn ex2 () {
+        assert_eq!(crate::day02::run_program(vec![1,0,0,0,99], 0), [2,0,0,0,99]);
+    }
+
+    #[test]
+    fn ex3 () {
+        assert_eq!(crate::day02::run_program(vec![2,3,0,3,99], 0), [2,3,0,6,99]);
+    }
+
+    #[test]
+    fn ex4 () {
+        assert_eq!(crate::day02::run_program(vec![2,4,4,5,99,0], 0), [2,4,4,5,99,9801]);
+    }
+
+    #[test]
+    fn ex5 () {
+        assert_eq!(crate::day02::run_program(vec![1,1,1,4,99,5,6,0,99], 0), [30,1,1,4,2,5,6,0,99]);
+    }
+}
+
+pub mod day02 {
+    pub fn run_program(mut v: Vec<usize>, index: usize) -> Vec<usize> {
+        if v[index] != 99 {
+            let new_index = v[index + 3];
+            
+            if v[index] == 1 {
+                v[new_index] = v[v[index + 1]] + v[v[index + 2]];
+            } else {
+                v[new_index] = v[v[index + 1]] * v[v[index + 2]];
+            }
+            return run_program(v, index + 4);
+        } else {
+            return v;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
-extern crate day01;
+extern crate day02;
 
 use std::fs;
 use std::time::{Instant};
@@ -10,13 +10,12 @@ use std::time::{Instant};
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let now = Instant::now();
 
-    let input: String  = fs::read_to_string("day01/input.data")?.parse()?;
-    let v: Vec<u32> = input.lines().map(|x| x.parse::<u32>().unwrap()).collect();
+    let input: String  = fs::read_to_string("day02/input.data")?.parse()?;
+    let v: Vec<usize> = input.split(",").map(|x| x.parse::<usize>().unwrap()).collect();
 
-    // let w:u32 = v.iter().map(|x| day01::day01::mass_to_fuel(*x)).sum();
-    let w:u32 = v.iter().map(|x| day01::day01::mass_to_fuel_rec(*x, 0)).sum();
+    let w = day02::day02::run_program(v, 0);
 
-    println!("{}", w);
+    println!("{}", w[0]);
     println!("took {} us", now.elapsed().as_micros());
     Ok(())
 }
