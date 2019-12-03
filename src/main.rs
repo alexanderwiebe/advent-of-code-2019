e3fe4243b4dc51955cfe34103b04bc8e159be1c4
aj <aj@softwarebyaj.com>
2019-12-03 05:58:56 +0000

    double for loops f t w

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,20 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let input: String  = fs::read_to_string("day02/input.data")?.parse()?;
     let v: Vec<usize> = input.split(",").map(|x| x.parse::<usize>().unwrap()).collect();
 
-    let w = day02::day02::run_program(v, 0);
-
-    println!("{}", w[0]);
+    'noun: for i in 1..100 {
+        'verb: for j in 1..100 {
+            let mut ww = v.to_vec();
+            ww[1] = i;
+            ww[2] = j;
+            let w = day02::day02::run_program(ww, 0);
+            if w[0] == 19690720 {
+                println!("i: {} j: {} ", i, j);
+                println!("{}", 100 * i + j);
+                break 'noun;
+            }
+        }
+    }
+    
     println!("took {} us", now.elapsed().as_micros());
     Ok(())
 }
