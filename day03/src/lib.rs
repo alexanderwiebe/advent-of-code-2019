f5696c33e42b8a334d3e57b2be54216fe97ec2ed
aj <aj@softwarebyaj.com>
2019-12-07 13:51:47 +0000

    solved day 3

---
+++ day03/src/lib.rs
@@ -0,0 +1,106 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test1 () {
+        let points = crate::day03::take_steps("D2", crate::day03::Point { x: 0, y: 0 });
+        
+        assert_eq!(points[0].x, 0 );
+        assert_eq!(points[0].y, -1 );
+        assert_eq!(points[1].x, 0 );
+        assert_eq!(points[1].y, -2 );
+    }
+
+    
+    #[test]
+    fn test2 () {
+        let points = crate::day03::take_steps("R2", crate::day03::Point { x: 0, y: 0 });
+        
+        assert_eq!(points[0].x, 1 );
+        assert_eq!(points[0].y, 0 );
+        assert_eq!(points[1].x, 2 );
+        assert_eq!(points[1].y, 0 );
+    }
+
+    fn test3 () {
+        let mut points1 = crate::day03::take_steps("R2", crate::day03::Point { x: 0, y: 0 });
+        points1.extend(crate::day03::take_steps("U2", crate::day03::Point { x: 2, y: 0 }));
+        let mut points2 = crate::day03::take_steps("U2", crate::day03::Point { x: 0, y: 0 });
+        points2.extend(crate::day03::take_steps("R2", crate::day03::Point { x: 0, y: 2 }));
+        let intersections = crate::day03::find_intersection(points1, points2);
+
+        assert_eq!(intersections.len(), 1);
+        assert_eq!(intersections[0].x, 2);
+        assert_eq!(intersections[0].y, 2);
+        
+    }
+
+    fn find_nearest_point() {
+        
+    }
+}
+
+pub mod day03 {
+    
+    #[derive(PartialEq, PartialOrd, Copy, Clone)]
+    pub struct Point {
+        pub x: isize,
+        pub y: isize
+    }
+
+    pub fn take_steps(instruction: &str, origin: Point) -> Vec<Point> {
+        let mut iter = instruction.chars();
+        let direction = iter.next().unwrap();
+        let steps: usize = iter.collect::<String>().parse().unwrap();
+
+        let mut points: Vec<Point> = Vec::new();
+
+        if direction == 'D' {
+            for step in 1..(steps + 1) {
+                points.push(Point { x: origin.x, y: origin.y - step as isize });
+            }
+        } else if direction == 'U' {
+            for step in 1..(steps + 1) {
+                points.push(Point { x: origin.x, y: origin.y + step as isize });
+            }
+        } else if direction == 'R' {
+            for step in 1..(steps + 1) {
+                points.push(Point { x: origin.x + step as isize, y: origin.y });
+            }
+        } else if direction == 'L' {
+            for step in 1..(steps + 1) {
+                points.push(Point { x: origin.x - step as isize, y: origin.y });
+            }
+        }
+
+        return points;
+    }
+
+    pub fn find_intersection(wire1: Vec<Point>, wire2: Vec<Point>) -> Vec<Point> {
+        let mut points: Vec<Point> = Vec::new();
+        for w1 in wire1.iter() {
+            for w2 in wire2.iter() {
+                if w1.x == w2.x && w1.y == w2.y && (w1.y != 0 && w2.y != 0 && w1.x != 0 && w2.x != 0) {
+                    points.push(*w1);
+                }
+            }
+        }
+        return points;
+    }
+
+    pub fn find_shortest_taxi(points: Vec<Point>) -> Point {
+        let mut point_iter = points.iter();
+        let mut max = point_iter.next().unwrap();
+        for p in point_iter {
+            if p.x.abs() + p.y.abs() < max.x.abs() + max.y.abs() {
+                max = p;
+            }
+        }
+        return *max;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
-extern crate day02;
+extern crate day03;
 
 use std::fs;
 use std::time::{Instant};
@@ -10,23 +10,40 @@ use std::time::{Instant};
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let now = Instant::now();
 
-    let input: String  = fs::read_to_string("day02/input.data")?.parse()?;
-    let v: Vec<usize> = input.split(",").map(|x| x.parse::<usize>().unwrap()).collect();
+    let input: String  = fs::read_to_string("day03/input.data")?.parse()?;
+    let wire1 = input.lines().nth(0);
+    let wire2 = input.lines().nth(1);
+    let v1: Vec<&str> = wire1.unwrap().split(",").collect::<Vec<&str>>();
+    let v2: Vec<&str> =  wire2.unwrap().split(",").collect::<Vec<&str>>();
 
-    'noun: for i in 1..100 {
-        'verb: for j in 1..100 {
-            let mut ww = v.to_vec();
-            ww[1] = i;
-            ww[2] = j;
-            let w = day02::day02::run_program(ww, 0);
-            if w[0] == 19690720 {
-                println!("i: {} j: {} ", i, j);
-                println!("{}", 100 * i + j);
-                break 'noun;
-            }
-        }
+    println!("read in");
+
+    let mut origin = day03::day03::Point { x: 0, y: 0 };
+    let mut points1: Vec<day03::day03::Point> = vec![];
+    for i in v1 {
+        let points = day03::day03::take_steps(i, origin);
+        origin = *points.last().unwrap();
+        points1.extend(points);
     }
+    println!("created points on v1");
+
+    origin = day03::day03::Point { x: 0, y: 0 };
+    let mut points2: Vec<day03::day03::Point> = vec![];
+    for i in v2 {
+        let points = day03::day03::take_steps(i, origin);
+        origin = *points.last().unwrap();
+        points2.extend(points);
+    }
+    println!("created points on v2");
+
     
+    let intersection = day03::day03::find_intersection(points1, points2);
+    println!("found intersections");
+
+    let shortest = day03::day03::find_shortest_taxi(intersection);
+
+    println!("shortest path is x: {}, y: {}", shortest.x, shortest.y);
+    println!("total distance is: {}", shortest.x.abs() + shortest.y.abs());
     println!("took {} us", now.elapsed().as_micros());
     Ok(())
 }
