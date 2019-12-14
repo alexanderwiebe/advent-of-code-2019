cbce06ed0775039ff9ed53858eb1206aa0e999b7
aj <aj@softwarebyaj.com>
2019-12-14 00:41:27 +0000

    finally fixed the conditions for day 4

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -20,12 +20,50 @@ mod tests {
     fn inc_false() {
         assert_eq!(crate::day04::inc_numb("123436"), false)
     }
+
+    #[test]
+    fn three_false() {
+        assert_eq!(crate::day04::three_adj_numb("123456"), false)
+    }
+
+    #[test]
+    fn three_true_double() {
+        assert_eq!(crate::day04::three_adj_numb("112233"), true)
+    }
+    #[test]
+    fn three_true_triple() {
+        assert_eq!(crate::day04::three_adj_numb("111466"), true)
+    }
+    #[test]
+    fn three_true_start_triple() {
+        assert_eq!(crate::day04::three_adj_numb("114666"), true)
+    }
+    #[test]
+    fn three_false_triple() {
+        assert_eq!(crate::day04::three_adj_numb("111666"), false)
+    }
+
+    #[test]
+    fn six_false_triple() {
+        assert_eq!(crate::day04::three_adj_numb("111111"), false)
+    }
+
+    #[test]
+    fn true_then_same() {
+        assert_eq!(crate::day04::three_adj_numb("112111"), true)
+    }
+    
+    #[test]
+    fn true_then_same_rev() {
+        assert_eq!(crate::day04::three_adj_numb("111211"), true)
+    }
+    
 }
 
 pub mod day04 {
     pub fn password_check(password: usize) -> bool {
         let password_str = password.to_string();
-        return two_adj_numb(&password_str) && inc_numb(&password_str);
+        return inc_numb(&password_str) && three_adj_numb(&password_str);
     }
 
     pub fn two_adj_numb(password: &str) -> bool {
@@ -37,12 +75,55 @@ pub mod day04 {
             if next == None { break }
             if letter.unwrap() == *next.unwrap() {
                 ret_val = true;
-                break;
             }
-
         }
         return ret_val;
     }
+
+    pub fn three_adj_numb(password: &str) -> bool {
+        let mut ret_val = false;
+        let mut loop_count = 0;
+        let mut iterator = password.chars().peekable();
+        loop {
+            let letter = iterator.next();
+            let next = iterator.peek();
+            if next == None { 
+                if loop_count == 1 {
+                    ret_val = true;
+                }
+                break;
+            }
+            if letter.unwrap() == *next.unwrap() {
+                loop_count += 1;
+                if loop_count > 1 { ret_val = false }
+            } else {
+                if loop_count == 1 {
+                    ret_val = true;
+                    break;
+                }
+                loop_count = 0;
+            }
+        }
+        return ret_val;
+    }
+
+    // pub fn three_adj_numb(password: &str) -> bool {
+    //     let mut ret_val = false;
+    //     let mut iterator = password.chars().peekable();
+    //     let mut letter = iterator.next();
+    //     loop {
+    //         let next = iterator.next();
+    //         if next == None { break }
+    //         if letter.unwrap() == next.unwrap() && (iterator.peek() == None || (iterator.peek() != None && next.unwrap() != *iterator.peek().unwrap())) {
+    //             ret_val = true;
+    //             break;
+    //         } else {
+    //             letter = iterator.next();
+    //             if letter == None { break }
+    //         }
+    //     }
+    //     return ret_val;
+    // }
 
     pub fn inc_numb(password: &str) -> bool {
         let mut ret_val = true;
