c66cebbee04c49bcc948d63e1a0793314e3e9ed3
aj <aj@softwarebyaj.com>
2019-12-15 06:11:23 +0000

    added start point for day5

---
+++ day05/src/lib.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+pub mod day05 {
+
+    pub fn start() {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,16 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
-extern crate day04;
+extern crate day05;
 
+use std::fs;
 use std::time::{Instant};
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let now = Instant::now();
-    let mut password_count = 0;
-    for password in 206938..679128 {
-        if day04::day04::password_check(password) {
-            password_count += 1;
-        }
-    }
 
-    println!("total passwords is {}", password_count);
+    let input: String  = fs::read_to_string("day02/input.data")?.parse()?;
+
     println!("took {} us", now.elapsed().as_micros());
     Ok(())
 }
